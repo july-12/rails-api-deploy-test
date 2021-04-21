ad117d6d24d05f1d72e627c1cafa1f092b9afa81
Faith <seventanyb@gmail.com>
2021-04-21 11:00:41 +0800

    amend deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
+# ask(:password, nil, echo: false)
+# server ENV["APP_SERVER"], user: ENV["APP_SERVER_USER"], port: 22, password: ENV["APP_SERVER_PASSWORD"], roles: [:web, :app, :db], primary: true
 server ENV["APP_SERVER"], roles: [:web, :app, :db], primary: true
 
 set :application, "rails-api-deploy-test"
@@ -9,26 +11,26 @@ set :user, ENV['APP_SERVER_USER']
 
 set :bundle_config, { deployment: false } 
 
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-set :puma_user,       fetch(:user)
-set :rbenv_custom_path, "/home/#{fetch(:user)}/.rbenv"
-# append :rbenv_map_bins, 'puma', 'pumactl'
-
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :rbenv_custom_path, "/home/#{fetch(:user)}/.rbenv"
+# append :rbenv_map_bins, 'puma', 'pumactl'
+
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -99,7 +101,7 @@ end
 append :linked_files, "config/database.yml", ".rbenv-vars"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
